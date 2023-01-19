4f38732de7f6fb820b916ba00ec906d0bd12e3e8
Hayden <hve0002@uah.edu>
2023-01-18 21:58:47 -0600

    maze puzzle

---
+++ HAYDEN_minotaur.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -m
+rm -rf MINOTAUR_SEARCH
+mkdir MINOTAUR_SEARCH 
+
+cd MINOTAUR_SEARCH
+
+PATHDEPTH=10
+PATHNUM=5
+
+for ((c=0; c<$PATHDEPTH; c++)) 
+do
+    for (( d=0; d<$PATHNUM; d++ ))
+    do
+	mkdir "path_$d"
+    done
+    declare x=$(shuf -i 0-4 -n 1)
+    cd "path_$x"
+done
+
+touch minotaur
+
+echo "./TAKIN_OUT_DA_TRASH/$FILENAME.c"
+
+exit
